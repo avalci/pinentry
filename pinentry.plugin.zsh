9a047a5cf1107457d49d2cabcf07c37c6aa7814a
Avalci Suzano <avalci@gmail.com>
2025-02-05 16:11:30 -0300

    first commit

---
+++ pinentry.plugin.zsh
@@ -0,0 +1,16 @@
+function pm-ssh-git-test {
+    ssh -T git@github.com 2>&1 | grep -q successfully && return 0 || return 1
+}
+
+function pm-ssh-add-list-test {
+    ssh-add -l 2>&1 > /dev/null && return 0 || return 1
+}
+
+alias gpg-agent='gpg-agent --pinentry-program "$(which pinentry-mac)"'
+export SSH_AUTH_SOCK=~/.gnupg/S.gpg-agent.ssh
+if ! pm-ssh-add-list-test ; then
+    killall -9 gpg-agent 2> /dev/null;
+    gpg-agent --daemon;
+fi
+while ! pm-ssh-add-list-test ; do echo; done
+while ! pm-ssh-git-test ; do echo; done
